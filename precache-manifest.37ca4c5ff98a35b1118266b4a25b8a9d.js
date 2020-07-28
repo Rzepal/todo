90694b28c987b30b345ddbd526007ff91a796965
Rzepal <max.rzepkowski@gmail.com>
2020-07-28 15:38:25 +0200

    Updates

---
+++ precache-manifest.37ca4c5ff98a35b1118266b4a25b8a9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3521617d0aad15b60dcd61cd188ed077",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "f9aa2634dc259e9d1a5a",
+    "url": "/todo/static/css/main.3c477b72.chunk.css"
+  },
+  {
+    "revision": "15f46739e8ea454074ff",
+    "url": "/todo/static/js/2.832108a6.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/todo/static/js/2.832108a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9aa2634dc259e9d1a5a",
+    "url": "/todo/static/js/main.54ecd534.chunk.js"
+  },
+  {
+    "revision": "3073a4d43d5bfcbc7714",
+    "url": "/todo/static/js/runtime-main.92367d58.js"
+  }
+]);
